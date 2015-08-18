cbd9bb3ade56c999fc78ae029976235e524a2496
Thomas Jakway <tjakway@gmail.com>
2015-08-18 16:24:26 -0700

    added CTORs to Song

--- src/main/java/com/jakway/music/song/Song.java
+++ src/main/java/com/jakway/music/song/Song.java
@@ -1,7 +1,12 @@
 package com.jakway.music.song;
 
 import com.jakway.music.song.SongKey;
+
+import java.io.File;
+
 import org.jaudiotagger.audio.AudioFile;
+import org.jaudiotagger.audio.AudioHeader;
+import org.jaudiotagger.tag.Tag;
 
 public class Song extends AudioFile
 {
@@ -9,6 +14,24 @@ public class Song extends AudioFile
     {
         return null;
     }
+
+    //subclasses don't inherit constructors
+    public Song()
+    {
+        super();
+    }
+    public Song(File f, AudioHeader audioHeader, Tag tag)
+    {
+        super(f, audioHeader, tag);
+    }
+
+    public Song(String s, AudioHeader audioHeader, Tag tag)
+    {
+        super(s, audioHeader, tag);
+    }
+
+
+    
 
     /**
      * WARNING: equals() MUST BE A DEEP COMPARISON FOR OBJECT EQUALITY, *NOT* A TEST IF THE SONGS MATCH!
