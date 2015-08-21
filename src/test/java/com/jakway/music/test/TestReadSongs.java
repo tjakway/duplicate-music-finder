41b493c0727d017c93c623e456dc4bb62845b57d
Thomas Jakway <tjakway@gmail.com>
2015-08-21 15:07:16 -0700

    added empty demo test

---
+++ src/test/java/com/jakway/music/test/TestReadSongs.java
@@ -0,0 +1,12 @@
+package com.jakway.music.test;
+
+import org.junit.*;
+
+public class TestReadSongs
+{
+    @Test
+    public void testExample()
+    {
+        System.out.println("hello, test!");
+    }
+}
